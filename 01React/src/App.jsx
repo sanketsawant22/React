5ada170ef72a6eaa1d779e6910835929351689c4
sanketsawant22 <162614467+sanketsawant22@users.noreply.github.com>
2024-08-25 23:08:10 +0530

    React File Structure

---
+++ 01React/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./Chai"
+
+function App() {
+    return (
+        <>
+            <h1>Hello React...!</h1>
+            <Chai />
+        </>
+    )
+}
+
+export default App
---
+++ 01React/src/Chai.jsx
@@ -0,0 +1,11 @@
+
+// In Vite files first letter must be capital
+// file must be jsx
+
+function Chai() {                   
+    return (
+        <h2>Chai Aur Code</h2>
+    )
+}
+
+export default Chai
